748cafe8477f7907d63b12ab5a97b457d036c2f1
shiva508 <dasarishiva1@gmail.com>
2024-06-04 09:55:49 +0530

    extra type safty

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -11,7 +11,7 @@ export class UserComponent {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
-  @Output() selected = new EventEmitter();
+  @Output() selected = new EventEmitter<string>();
 
   onSelectUser() {
     this.selected.emit(this.id);
