112ad06e326d1700657a16a3529c95a38b14077a
shiva508 <dasarishiva1@gmail.com>
2024-06-04 09:12:37 +0530

    input with signals

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, computed, input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,12 +8,9 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
-
+  //avatar = input<string>();
+  avatar = input.required<string>();
+  name = input.required<string>();
   onSelectUser() {}
-
-  get imagePath() {
-    return 'assets/users/' + this.avatar;
-  }
+  imagePath = computed(() => 'assets/users/' + this.avatar());
 }
