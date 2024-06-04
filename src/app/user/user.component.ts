6f83f3d3dc2cfcf057980eb8d10fc13efc1e5c46
shiva508 <dasarishiva1@gmail.com>
2024-06-04 06:23:46 +0530

    signals usage

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 @Component({
@@ -9,13 +9,16 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[1];
+  selectedUser = signal(DUMMY_USERS[1]);
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
 
-  get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar;
-  }
+  // get imagePath() {
+  //   return 'assets/users/' + this.selectedUser().avatar;
+  // }
 
   onSelectUser() {
+    this.selectedUser().name = 'Unknown';
+    this.selectedUser.set(DUMMY_USERS[2]);
     console.log('Clicked');
   }
 }
