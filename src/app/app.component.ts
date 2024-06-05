af3f94aae0d296be3f8702ec00ce9b03402479ba
shiva508 <dasarishiva1@gmail.com>
2024-06-05 08:20:39 +0530

    typescript errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,24 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'dear-comrade-angular-zero-to-hero';
   users = DUMMY_USERS;
+  userId = '';
+  userName: string = '';
+
   onSelectUser(id: string) {
     console.log(id);
+    this.userId = id;
+    this.userName = DUMMY_USERS.find((user) => user.id === this.userId)?.name!;
   }
 }
